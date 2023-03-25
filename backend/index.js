fec25c7871f4c2af56172283abbc9f55363c3b32
Saad Khan <saadahmedkhan552000@gmail.com>
2023-03-25 19:04:20 +0500

    Initial commit

---
+++ backend/index.js
@@ -0,0 +1,13 @@
+function downloadBlob(content, filename, contentType) {
+  // Create a blob
+  var blob = new Blob([content], { type: contentType });
+  var url = URL.createObjectURL(blob);
+
+  // Create a link to download it
+  var pom = document.createElement('a');
+  pom.href = url;
+  pom.setAttribute('download', filename);
+  pom.click();
+}
+
+module.export = downloadBlob;
